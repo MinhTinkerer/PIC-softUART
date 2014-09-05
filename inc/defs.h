9ee496012fccef124bd5e4fe1e95f7de0f9d231f
Rafael Dias <rdmeneze@gmail.com>
2014-09-05 00:06:04 -0300

    - adicao de arquivos

---
+++ inc/defs.h
@@ -0,0 +1,26 @@
+/* 
+ * File:   defs.h
+ * Author: rafael dias <rafael.dias@silicaconsulting.com>
+ *
+ * Created on September 3, 2014, 10:27 PM
+ */
+
+#ifndef DEFS_H
+#define	DEFS_H
+
+#include <stdint.h>
+
+#ifdef	__cplusplus
+extern "C" {
+#endif
+
+typedef void* LPVOID;
+typedef unsigned char BYTE;
+typedef unsigned int  WORD;
+
+#ifdef	__cplusplus
+}
+#endif
+
+#endif	/* DEFS_H */
+
---
+++ src/interruptions.c
@@ -0,0 +1,46 @@
+
+/******************************************************************************/
+/*Files to Include                                                            */
+/******************************************************************************/
+
+#if defined(__XC)
+    #include <xc.h>         /* XC8 General Include File */
+#elif defined(HI_TECH_C)
+    #include <htc.h>        /* HiTech General Include File */
+#elif defined(__18CXX)
+    #include <p18cxxx.h>    /* C18 General Include File */
+#endif
+
+#if defined(__XC) || defined(HI_TECH_C)
+
+#include <stdint.h>         /* For uint8_t definition */
+#include <stdbool.h>        /* For true/false definition */
+
+#endif
+
+/******************************************************************************/
+/* Interrupt Routines                                                         */
+/******************************************************************************/
+
+/* High-priority service */
+
+void interrupt high_isr(void)
+{
+
+      /* This code stub shows general interrupt handling.  Note that these
+      conditional statements are not handled within 3 seperate if blocks.
+      Do not use a seperate if block for each interrupt flag to avoid run
+      time errors. */
+
+}
+
+/* Low-priority interrupt routine */
+void low_priority interrupt low_isr(void)
+{
+
+      /* This code stub shows general interrupt handling.  Note that these
+      conditional statements are not handled within 3 seperate if blocks.
+      Do not use a seperate if block for each interrupt flag to avoid run
+      time errors. */
+
+}
